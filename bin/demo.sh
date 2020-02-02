f9a62086df718f3cd6ee5641e53d2c6b631c25ee
Brian Buchalter <brian.buchalter@gusto.com>
2020-02-01 20:04:06 -0700

    Improve demo script

--- bin/demo.sh
+++ bin/demo.sh
@@ -22,25 +22,22 @@ setup_new_tmux_session() {
 	read -p "Press enter here when ready" ready
 }
 
-ruby_sleep_2_seconds_ruby_sleep_2_seconds() {
-	tmux select-pane -t 1
-	tmux send-keys -t $SESSION_NAME "time curl http://localhost:3000/ruby_sleep_2_seconds"  C-m
-	tmux select-pane -t 2
-	tmux send-keys -t $SESSION_NAME "time curl http://localhost:3000/ruby_sleep_2_seconds"  C-m
+ruby_sleep_2_seconds() {
+	PANE=$1
+	tmux select-pane -t $PANE
+	tmux send-keys -t $SESSION_NAME "time curl http://localhost:3000/ruby_sleep_2_seconds"  C-m C-m
 }
 
-ruby_sleep_2_seconds_c_sleep_2_seconds() {
-	tmux select-pane -t 1
-	tmux send-keys -t $SESSION_NAME "time curl http://localhost:3000/ruby_sleep_2_seconds"  C-m
-	tmux select-pane -t 2
-	tmux send-keys -t $SESSION_NAME "time curl http://localhost:3000/c_sleep_2_seconds"  C-m
+c_sleep_2_seconds_with_gil() {
+	PANE=$1
+	tmux select-pane -t $PANE
+	tmux send-keys -t $SESSION_NAME "time curl http://localhost:3000/c_sleep_2_seconds_with_gil"  C-m C-m
 }
 
-c_sleep_2_seconds_c_sleep_2_seconds() {
-	tmux select-pane -t 1
-	tmux send-keys -t $SESSION_NAME "time curl http://localhost:3000/c_sleep_2_seconds"  C-m
-	tmux select-pane -t 2
-	tmux send-keys -t $SESSION_NAME "time curl http://localhost:3000/c_sleep_2_seconds"  C-m
+c_sleep_2_seconds_without_gil() {
+	PANE=$1
+	tmux select-pane -t $PANE
+	tmux send-keys -t $SESSION_NAME "time curl http://localhost:3000/c_sleep_2_seconds_without_gil"  C-m C-m
 }
 
 cleanup() {
@@ -51,24 +48,40 @@ cleanup() {
 }
 
 demo_loop() {
+	clear
 	echo "Which demo would you like?"
 	echo "1) ruby_sleep_2_seconds && ruby_sleep_2_seconds"
-	echo "2) ruby_sleep_2_seconds && c_sleep_2_seconds"
-	echo "3) c_sleep_2_seconds && c_sleep_2_seconds"
+	echo "2) c_sleep_2_seconds_with_gil && c_sleep_2_seconds_with_gil"
+	echo "3) c_sleep_2_seconds_without_gil && c_sleep_2_seconds_without_gil"
+	echo "4) c_sleep_2_seconds_without_gil && c_sleep_2_seconds_with_gil"
+	echo "5) c_sleep_2_seconds_with_gil && c_sleep_2_seconds_without_gil"
 	echo "9) exit"
 	read -p "Choose number: " demo_number
 
 	case $demo_number in
 		"1")
-			ruby_sleep_2_seconds_ruby_sleep_2_seconds
+			ruby_sleep_2_seconds "1"
+			ruby_sleep_2_seconds "2"
 			demo_loop
 			;;
 		"2")
-			ruby_sleep_2_seconds_c_sleep_2_seconds
+			c_sleep_2_seconds_with_gil "1"
+			c_sleep_2_seconds_with_gil "2"
 			demo_loop
 			;;
 		"3")
-			c_sleep_2_seconds_c_sleep_2_seconds
+			c_sleep_2_seconds_without_gil "1"
+			c_sleep_2_seconds_without_gil "2"
+			demo_loop
+			;;
+		"4")
+			c_sleep_2_seconds_without_gil "1"
+			c_sleep_2_seconds_with_gil "2"
+			demo_loop
+			;;
+		"5")
+			c_sleep_2_seconds_with_gil "1"
+			c_sleep_2_seconds_without_gil "2"
 			demo_loop
 			;;
 		"9")
